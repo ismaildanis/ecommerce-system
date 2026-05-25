9bc944d79601f142a95548b23e9648e4f0d0a543
Hecksoft0 <hecksoft0@gmail.com>
2026-05-25 04:19:25 +0300

    update

--- backend/app/Http/Controllers/Api/Seller/VariantController.php
+++ backend/app/Http/Controllers/Api/Seller/VariantController.php
@@ -15,35 +15,35 @@ class VariantController extends Controller
         private readonly ProductVariantService $variantService
     ) {}
 
-    public function index($productId)
+    public function index(int $productId)
     {
         $variants = $this->variantService->index($productId);
 
         return Response::json(ProductVariantResource::collection($variants));
     }
 
-    public function store(StoreProductVariantRequest $request, $productId)
+    public function store(StoreProductVariantRequest $request, int $productId)
     {
         $variant = $this->variantService->store($request->all(), $productId);
 
         return Response::json(new ProductVariantResource($variant));
     }
 
-    public function show($productId, $id)
+    public function show(int $productId, int $id)
     {
         $variant = $this->variantService->show($productId, $id);
 
         return Response::json(new ProductVariantResource($variant));
     }
 
-    public function update(UpdateProductVariantRequest $request, $productId, $id)
+    public function update(UpdateProductVariantRequest $request, int $productId, int $id)
     {
         $variant = $this->variantService->update($productId, $id, $request->all());
 
         return Response::json(new ProductVariantResource($variant));
     }
 
-    public function destroy($productId, $id)
+    public function destroy(int $productId, int $id)
     {
         $variant = $this->variantService->destroy($productId, $id);
 
